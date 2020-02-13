0ac873a2e53b35747119bb9e03d4b0424dedaf6e
GG Guitart <gertaguitart2019@u.northwestern.edu>
2020-02-13 15:09:45 -0600

    trying to connect frontend and back end but running into errors

---
+++ crowdsourcing-second/client/src/pages/VideosInsert.jsx
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+class VideosInsert extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see the form to add a movies</p>
+            </div>
+        )
+    }
+}
+
+export default VideosInsert
---
+++ crowdsourcing-second/client/src/pages/index.js
@@ -0,0 +1,6 @@
+
+import VideosList from './VideosList'
+import VideosInsert from './VideosInsert'
+import VideosUpdate from './VideosUpdate'
+
+export { VideosList, VideosInsert, VideosUpdate }
