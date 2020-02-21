a5df717e5a5628e60827014a7f5c1e576d01b956
GG Guitart <gertaguitart2019@u.northwestern.edu>
2020-02-22 00:18:57 +0100

    commented shit out

--- crowdsourcing-second/client/src/pages/VideosList.jsx
+++ crowdsourcing-second/client/src/pages/VideosList.jsx
@@ -4,83 +4,83 @@
 import React, { Component } from 'react'
 import { getAllVideos } from '../api'
 //  import Table from 'react'
-import api from '../api'
-import apis from '../api'
-import ReactTable from 'react-table'
+// import api from '../api'
+ import apis from '../api'
+// import ReactTable from 'react-table'
 
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-import 'react-table/react-table.css'
+// import 'react-table/react-table.css'
 
-const Wrapper = styled.div
-`padding: 0 40px 40px 40px`
-
+// const Wrapper = styled.div
+// `padding: 0 40px 40px 40px`
+//   run = async () => {
+//         const videoslisthopefully = await apis.getAllVideos();
+//         console.log("this is from the run() function" + videoslisthopefully.data)
+//     };
+    //  run();
 
 class VideosList extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            videos: [],
-            columns: [],
-            isLoading: false
-        }
-        console.log("are the props showing up" + props)
-    }
-
-    componentDidMount = async () => {
-        this.setState({isLoading: true})
-        await apis.getAllVideos().then(videos => {
-            this.setState({
-                videos: videos.data,
-                isLoading: false,
-            })
-        })
-        console.log("comp did mount " + api.getAllVideos())
-    };
-
-    // run = async () => {
-    //     const videoslisthopefully = await apis.getAllVideos();
-    //     console.log("this is from the run() function" + videoslisthopefully.data)
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         videos: [],
+    //         columns: [],
+    //         isLoading: false
+    //     }
+    //     console.log("are the props showing up" + props)
     // }
-    // //  run()
+
+    // componentDidMount = async () => {
+    //     this.setState({isLoading: true})
+    //     await apis.getAllVideos().then(videos => {
+    //         this.setState({
+    //             videos: videos.data,
+    //             isLoading: false,
+    //         })
+    //     })
+    //     console.log("comp did mount " + api.getAllVideos())
+    // };
+
+  
+
 
     render() {
-        const {videos, isLoading} = this.state
-        console.log ('is this working ha', videos)
-        const columns = [
-            {
-                Header:'ID',
-                accessor:'_id',
-                filterable:true
-            },
-            {
-                Header:'papername',
-                accessor:'paperName',
-                filterable:true
-            }
-        ]
-
-        let showTable = true
-        if (!videos.length) {
-            showTable = false
-        }
-
-        return (
+        return(
             <div>
             <p>In this page you'll see the form to add a videos</p>
-        
-        <Wrapper>
-            {showTable &&(
-                <ReactTable
-                data={videos}
-                columns={columns}
-                loading={isLoading}/>
-            )}
-        </Wrapper></div>
+        </div>
         )
     }
    
 }
 
     export default VideosList
+    
+    // const {videos, isLoading} = this.state
+        // console.log ('is this working ha', videos)
+        // const columns = [
+        //     {
+        //         Header:'ID',
+        //         accessor:'_id',
+        //         filterable:true
+        //     },
+        //     {
+        //         Header:'papername',
+        //         accessor:'paperName',
+        //         filterable:true
+        //     }
+        // ]
+
+        // let showTable = true
+        // if (!videos.length) {
+        //     showTable = false
+        // } // {/* <Wrapper>
+        //     {showTable &&(
+        //         <ReactTable
+        //         data={videos}
+        //         columns={columns}
+        //         loading={isLoading}/>
+        //     )}
+        // </Wrapper></div> */}
